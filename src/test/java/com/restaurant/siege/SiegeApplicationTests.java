76b0b4c9018f9a0cee92d3edea8d75ac3d35b48a
Naitive13 <hei.titan.2@gmail.com>
2025-04-18 10:00:04 +0300

    first commit

---
+++ src/test/java/com/restaurant/siege/SiegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.siege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
