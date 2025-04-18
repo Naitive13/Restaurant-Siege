1fbda83f4153974eacc4c119852a8715486b1cfb
Naitive13 <hei.titan.2@gmail.com>
2025-04-18 16:04:10 +0300

    feat: update implementation of DashboardService::getBestSales

--- src/main/java/com/restaurant/siege/service/DashboardService.java
+++ src/main/java/com/restaurant/siege/service/DashboardService.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.restaurant.siege.model.DurationType;
 import com.restaurant.siege.model.ProcessingTimeType;
 import com.restaurant.siege.model.entities.DishOrder;
+import com.restaurant.siege.model.entities.DishOrderStatus;
 import com.restaurant.siege.model.entities.ProcessingTime;
 import com.restaurant.siege.model.entities.SoldDish;
 import java.net.URI;
@@ -31,70 +32,54 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class DashboardService {
-    private final DishOrderDAO dishOrderDAO;
+  private final DishOrderDAO dishOrderDAO;
 
   public List<SoldDish> getBestSales(LocalDateTime dateMin, LocalDateTime dateMax) {
-    //    List<DishOrder> dishOrders = dishOrderDAO.getAll(1, 100);
-    throw new UnsupportedOperationException("not done yet");
-    //    try (HttpClient client = HttpClient.newHttpClient()) {
-    //      HttpRequest request = HttpRequest.newBuilder().uri(new
-    // URI("http://localhost:8081")).build();
-    //      HttpResponse<String> response = client.send(request,
-    // HttpResponse.BodyHandlers.ofString());
-    //      ObjectMapper objectMapper = new ObjectMapper();
-    //
-    //      List<DishOrder> dishOrders =
-    //          objectMapper.readValue(response.body(), new TypeReference<List<DishOrder>>() {});
-    //
-    //      List<DishOrder> finished =
-    //          dishOrders.stream()
-    //              .filter(
-    //                  dishOrder ->
-    //                      dishOrder.getStatusList().stream()
-    //                          .anyMatch(dishOrderStatus ->
-    // dishOrderStatus.getStatus().equals(DONE)))
-    //              .toList();
-    //      List<DishOrder> dishOrdersBetweenInterval =
-    //          finished.stream()
-    //              .filter(
-    //                  dishOrder ->
-    //                      (dishOrder.getActualStatus().getCreationDate().isAfter(dateMin)
-    //                          && dishOrder.getActualStatus().getCreationDate().isBefore(dateMax)))
-    //              .toList();
-    //
-    //      List<SoldDish> soldDishes = new ArrayList<>();
-    //      dishOrdersBetweenInterval.forEach(
-    //          dishOrder -> {
-    //            if (soldDishes.stream()
-    //                .anyMatch(soldDish -> soldDish.getDishName().equals(dishOrder.getDishName())))
-    // {
-    //              for (int i = 0; i < soldDishes.size(); i++) {
-    //                if (soldDishes.get(i).getDishName().equals(dishOrder.getDishName())) {
-    //                  Double profit = soldDishes.get(i).getTotalProfit();
-    //                  soldDishes.get(i).setTotalProfit(profit + dishOrder.getTotalAmount());
-    //                  Long quantity = soldDishes.get(i).getTotalQuantities();
-    //                  soldDishes.get(i).setTotalQuantities(quantity + dishOrder.getQuantity());
-    //                }
-    //              }
-    //            } else {
-    //              SoldDish soldDish = new SoldDish();
-    //
-    //              soldDish.setDishName(dishOrder.getDishName());
-    //              soldDish.setTotalProfit(dishOrder.getTotalAmount().doubleValue());
-    //              soldDish.setTotalQuantities((long) dishOrder.getQuantity());
-    //
-    //              soldDishes.add(soldDish);
-    //            }
-    //          });
-    //
-    //      return soldDishes.stream()
-    //          .sorted(Comparator.comparing(SoldDish::getTotalQuantities,
-    // Comparator.naturalOrder()))
-    //          .toList();
-    //
-    //    } catch (Exception e) {
-    //      throw new RuntimeException(e);
-    //    }
+    List<DishOrder> dishOrders = dishOrderDAO.getAll(1, 100);
+    List<DishOrder> finished =
+        dishOrders.stream()
+            .filter(
+                dishOrder ->
+                    dishOrder.getStatusList().stream()
+                        .anyMatch(dishOrderStatus -> dishOrderStatus.getStatus().equals(DONE)))
+            .toList();
+    List<DishOrder> dishOrdersBetweenInterval =
+        finished.stream()
+            .filter(dishOrder -> {
+              if (dateMax != null && dateMin != null){
+                return filterDishOrder(dateMin, dateMax, dishOrder);
+              }
+              return true;
+            })
+            .toList();
+
+    List<SoldDish> soldDishes = new ArrayList<>();
+    dishOrdersBetweenInterval.forEach(
+        dishOrder -> {
+          if (soldDishes.stream()
+              .anyMatch(soldDish -> soldDish.getDishName().equals(dishOrder.getDishName()))) {
+            for (int i = 0; i < soldDishes.size(); i++) {
+              if (soldDishes.get(i).getDishName().equals(dishOrder.getDishName())) {
+                Double profit = soldDishes.get(i).getTotalProfit();
+                soldDishes.get(i).setTotalProfit(profit + dishOrder.getTotalAmount());
+                Long quantity = soldDishes.get(i).getTotalQuantities();
+                soldDishes.get(i).setTotalQuantities(quantity + dishOrder.getQuantity());
+              }
+            }
+          } else {
+            SoldDish soldDish = new SoldDish();
+
+            soldDish.setDishName(dishOrder.getDishName());
+            soldDish.setTotalProfit(dishOrder.getTotalAmount().doubleValue());
+            soldDish.setTotalQuantities((long) dishOrder.getQuantity());
+
+            soldDishes.add(soldDish);
+          }
+        });
+
+    return soldDishes.stream()
+        .sorted(Comparator.comparing(SoldDish::getTotalQuantities, Comparator.naturalOrder()))
+        .toList();
   }
 
   public ProcessingTime getProcessingTimeFor(
@@ -167,11 +152,12 @@ public class DashboardService {
     String urls = System.getenv("SALES_POINT_URLS");
     List<String> salesPoints = Arrays.asList(urls.split(","));
     log.info(salesPoints.toString());
-    salesPoints.forEach(url -> {
-      log.info(url);
-      List<DishOrder> dishOrders = fetchFromRestaurant(url);
-      dishOrderDAO.saveAll(dishOrders);
-    });
+    salesPoints.forEach(
+        url -> {
+          log.info(url);
+          List<DishOrder> dishOrders = fetchFromRestaurant(url);
+          dishOrderDAO.saveAll(dishOrders);
+        });
   }
 
   private List<DishOrder> fetchFromRestaurant(String restaurantURL) {
@@ -188,5 +174,18 @@ public class DashboardService {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private boolean filterDishOrder(
+      LocalDateTime dateMin, LocalDateTime dateMax, DishOrder dishOrder) {
+    DishOrderStatus actualStatus =
+        dishOrder.getStatusList().stream()
+            .filter(
+                dishOrderStatus -> dishOrder.getActualStatus().equals(dishOrderStatus.getStatus()))
+            .findFirst()
+            .get();
+
+    return actualStatus.getCreationDate().isAfter(dateMin)
+        && actualStatus.getCreationDate().isBefore(dateMax);
   }
 }
