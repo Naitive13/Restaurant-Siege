966ca848df5fafd2a55b3b6e369432da5755a77f
Naitive13 <hei.titan.2@gmail.com>
2025-04-19 08:03:44 +0300

    feat: update class ProcessingTime according to the specification

--- src/main/java/com/restaurant/siege/model/entities/ProcessingTime.java
+++ src/main/java/com/restaurant/siege/model/entities/ProcessingTime.java
@@ -1,14 +1,17 @@
 package com.restaurant.siege.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.restaurant.siege.model.DurationType;
-import com.restaurant.siege.model.ProcessingTimeType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ProcessingTime {
-    private ProcessingTimeType processingTimeType;
-    private DurationType durationType;
-    private Long value;
+    private String salesPoint;
+    private String dish;
+    private Long preparationDuration;
+    private DurationType durationUnit;
+    @JsonIgnore
+    private DishOrder dishOrder;
 }
